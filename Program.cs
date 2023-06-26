94df0c1b787fe9e38d773da638d0a11042704edd
BhagyashriSabale <balisabale16@gmail.com>
2023-06-26 12:17:27 +0530

    [Bhagyashri] Welcome Message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PracticeProblemDay2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Practice Problem Day 2");
+            
+        }
+    }
+}
