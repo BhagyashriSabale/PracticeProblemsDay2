4c88a8108d3636aee1b918a7da01dd49639305ad
BhagyashriSabale <balisabale16@gmail.com>
2023-06-26 12:25:39 +0530

    [Bhagyashri] Check given numbers equal or not

---
+++ NumbersEqualorNot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblemDay2
+{
+    internal class NumbersEqualorNot
+    {
+        public void num()
+        {
+            Console.WriteLine("Enter the first num: ");
+            int n1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second num: ");
+            int n2=Convert.ToInt32(Console.ReadLine());
+            if(n1==n2)
+            {
+                Console.WriteLine("The two integers are equal.");
+            }
+            else
+            {
+                Console.WriteLine("The two integers are not equal.");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Practice Problem Day 2");
+            Console.WriteLine("Please Choose one program from below list");
+            Console.WriteLine("\n1.NumbersEqualorNot");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    NumbersEqualorNot number = new NumbersEqualorNot();
+                    number.num();
+                    break;
+            }
             
         }
     }
