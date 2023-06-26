5545f576c52c795154c453e8bf0fb139288bbac8
BhagyashriSabale <balisabale16@gmail.com>
2023-06-26 12:39:52 +0530

    [Bhagyashri] Check given age of candidate is eligible for voting or not

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome Practice Problem Day 2");
             Console.WriteLine("Please Choose one program from below list");
-            Console.WriteLine("\n1.NumbersEqualorNot\n2.Evenodd");
+            Console.WriteLine("\n1.NumbersEqualorNot\n2.Evenodd\n3.VotingAge");
             int option=Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -17,6 +17,10 @@
                 case 2:
                     Evenodd evenodd = new Evenodd();    
                     evenodd.EvenOdd();
+                    break;
+                case 3:
+                    VotingAge3 vote=new VotingAge3();
+                    vote.AgeVote();
                     break;
             }
             
---
+++ VotingAge3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblemDay2
+{
+    internal class VotingAge3
+    {
+        public void AgeVote()
+        {
+            Console.WriteLine("Enter the age of Candidate: ");
+            int age=Convert.ToInt32(Console.ReadLine());
+            if(age>=18)
+            {
+                Console.WriteLine("the candidate is eligible for voting.");
+            }
+            else
+            {
+                Console.WriteLine("The candidate is not eligible for voting.");
+            }
+        }
+    }
+}
