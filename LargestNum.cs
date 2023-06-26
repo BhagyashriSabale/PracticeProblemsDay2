6f65d66d0c894bc533357d6c316a39bfc895a02f
BhagyashriSabale <balisabale16@gmail.com>
2023-06-26 17:49:10 +0530

    [Bhagyashri] Find largest number

---
+++ LargestNum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblemDay2
+{
+    internal class LargestNum
+    {
+        public void LargestNumber()
+        {
+            Console.WriteLine("Enter the first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("Number one is the largest!");
+                }
+                else
+                {
+                    Console.Write("Number three is the largest!");
+                }
+            }
+            else if (num2 > num3)
+            {
+                Console.Write("Number two is the largest!");
+            }
+            else
+            {
+                Console.Write("Number three is the largest!");
+            }
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome Practice Problem Day 2");
             Console.WriteLine("Please Choose one program from below list");
-            Console.WriteLine("\n1.NumbersEqualorNot\n2.Evenodd\n3.VotingAge\n4.ArithmeticOperations\n5.WeekDaywithWeekdayNumber\n6.Factorial\n7.Powerof2\n8.SumofSquare\n9.ReverseWord\n10.SomOfNaturalNumbers");
+            Console.WriteLine("\n1.NumbersEqualorNot\n2.Evenodd\n3.VotingAge\n4.ArithmeticOperations\n5.WeekDaywithWeekdayNumber\n6.Factorial\n7.Powerof2\n8.SumofSquare\n9.ReverseWord\n10.SomOfNaturalNumbers\n11.LargestNumber");
             int option=Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -49,6 +49,10 @@
                 case 10:
                     SumofnaturalNumbers Sum=new SumofnaturalNumbers();
                     Sum.sumofFirst5NaturalNumbers();
+                    break;
+                case 11:
+                    LargestNum large=new LargestNum();
+                    large.LargestNumber();
                     break;
 
             }
