3d4051c1e903fc63dcdacb843e9ccb9443e9fe4e
BhagyashriSabale <balisabale16@gmail.com>
2023-06-26 13:29:44 +0530

    [Bhagyashri] Find factorial

---
+++ Factorial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblemDay2
+{
+    internal class Factorial
+    {
+        public void Fact()
+        {
+            Console.WriteLine("Enter a number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int factorial = 1;
+            for(int i=1; i<=n; i++) 
+            { 
+            
+                factorial *= i;
+               
+            }
+
+            Console.WriteLine($"{n} factorial = {factorial}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome Practice Problem Day 2");
             Console.WriteLine("Please Choose one program from below list");
-            Console.WriteLine("\n1.NumbersEqualorNot\n2.Evenodd\n3.VotingAge\n4.ArithmeticOperations\n5.WeekDaywithWeekdayNumber");
+            Console.WriteLine("\n1.NumbersEqualorNot\n2.Evenodd\n3.VotingAge\n4.ArithmeticOperations\n5.WeekDaywithWeekdayNumber\n6.Factorial");
             int option=Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -29,6 +29,10 @@
                 case 5:
                     Weekday weekday=new Weekday();
                     weekday.WeekdayNumber();
+                    break;
+                case 6:
+                    Factorial factorial=new Factorial();
+                    factorial.Fact();
                     break;
             }
             
